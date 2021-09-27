964a7aebee9293f8b5a5b3f012dc34af4da3e0d9
MusharratTabassum <musharrattabassum710@gmail.com>
2021-09-27 23:32:49 +0600

    more functionality added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,16 +3,18 @@ import './Cart.css';
 
 const Cart = (props) => {
     const { cart } = props;
-    let total = 0;
+    let total = 0, Item;
 
     for (const developer of cart) {
         total = total + developer.fee;
+        Item = developer.name;
     }
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items Ordered: {props.cart.length}</h5>
             <p>Total: ${total.toFixed(2)}</p>
+            <h5>Item Name : {Item}</h5>
 
         </div>
     );
